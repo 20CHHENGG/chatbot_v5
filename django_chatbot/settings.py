b1a356530b74389af7eb5258fe5480abc1e30efe
chheng <chheng@gmail.com>
2023-08-27 22:01:53 +0700

    somthing change commit

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -123,6 +123,7 @@ MEDIA_URL='images/'
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'static/images/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
